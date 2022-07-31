74f62f9b55dbf45558b3db11d6a0dc2d994e80b0
scarf <greenscarf005@gmail.com>
2022-07-31 11:42:37 +0900

    chore: format

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -1,30 +1,25 @@
-import akaneAvatar from "../assets/akane.svg";
-import eliotAvatar from "../assets/eliot.svg";
-import emilyAvatar from "../assets/emily.svg";
-import joeAvatar from "../assets/joe.svg";
+import akaneAvatar from '../assets/akane.svg'
+import eliotAvatar from '../assets/eliot.svg'
+import emilyAvatar from '../assets/emily.svg'
+import joeAvatar from '../assets/joe.svg'
 
 export const akaneModel = {
-    name: "Akane",
-    avatar: akaneAvatar
-}; 
+  name: 'Akane',
+  avatar: akaneAvatar,
+}
 
 export const eliotModel = {
-    name: "Eliot",
-    avatar: eliotAvatar
-}; 
+  name: 'Eliot',
+  avatar: eliotAvatar,
+}
 
 export const emilyModel = {
-    name: "Emily",
-    avatar: emilyAvatar
-};
+  name: 'Emily',
+  avatar: emilyAvatar,
+}
 
 export const joeModel = {
-    name: "Joe",
-    avatar: joeAvatar
-}; 
-export const users = [
-    akaneModel,
-    eliotModel,
-    emilyModel,
-    joeModel  
-];
+  name: 'Joe',
+  avatar: joeAvatar,
+}
+export const users = [akaneModel, eliotModel, emilyModel, joeModel]
